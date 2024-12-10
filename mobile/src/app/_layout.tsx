21543c9c87ca338f6a15d378cb799fae89f75c49
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-12-10 00:17:08 -0300

    aula 1

---
+++ mobile/src/app/_layout.tsx
@@ -0,0 +1,26 @@
+import { Stack } from "expo-router";
+import { colors } from "@/styles/theme";
+
+import { useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold, } from "@expo-google-fonts/rubik";
+
+import { Loading } from "@/components/loading";
+
+export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Rubik_600SemiBold,
+        Rubik_400Regular,
+        Rubik_500Medium,
+        Rubik_700Bold,
+    })
+
+    if (!fontsLoaded) {
+        return <Loading />
+    }
+
+    return <Stack 
+    screenOptions={{
+        headerShown: false, // "headerShown: false" -> tira o cabeçalho padrão da tela
+        contentStyle: { backgroundColor: colors.gray[100] },
+    }}
+    />
+}
