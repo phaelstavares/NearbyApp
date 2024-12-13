3c44b66a1067c78169ef82f5cc0b10d35d890f71
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-12-12 23:48:08 -0300

    id

--- mobile/src/app/market/[id].tsx
+++ mobile/src/app/market/[id].tsx
@@ -1,16 +1,41 @@
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { View, Alert } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { api } from "@/services/api";
+import { useEffect, useState } from "react";
+
+import { Loading } from "@/components/loading";
 
 export default function Market() {
+    const [data, setData] = useState()
+    const [isLoading, setIsLoading] = useState(true)
+
     const params = useLocalSearchParams<{ id: string }>()
 
     async function fetchMarket() {
-        
+        try {
+            const { data } = await api.get("/markets/" + params.id)
+            setData(data)
+            setIsLoading(false)
+            
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Erro", "Não foi possível carregar os dados", [
+                { text: "Ok", onPress: () => router.back() },
+            ])
+        }
+    }
+
+    useEffect(() => {
+        fetchMarket()
+    }, [params.id])
+
+    if (isLoading) {
+        return <Loading />
     }
 
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center", }}>
-            <Text>{params.id}</Text>
+            
         </View>
     )
 }
